5b3d6d5170a6a9f6c1ddbac2a31244672f7ac4ab
GPegov <66030738+GPegov@users.noreply.github.com>
2023-09-14 12:55:52 +0500

    Добавил анимации на список гостей.

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,15 @@
 <template>
     <div v-if="posts.length > 0">
         <h3 class="registered">Зарегистрированные гости</h3>
-        <PostItem v-for = 'post in posts'
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    />
+        <transition-group name="user-list">
+            <PostItem 
+                v-for = 'post in posts'
+                :post="post"
+                :key="post.id"
+                @remove="$emit('remove', post)"
+            />
+        </transition-group>
+    
     </div>
     <h2 v-else style="
     color: rgb(135, 38, 165);
@@ -35,5 +39,21 @@ export default {
 <style scoped>
 .registered {
     margin-top: 15px;
+}
+.user-list {
+    display: inline-block;
+    margin-right: 10px;
+}
+.user-list-enter-active,
+.user-list-leave-active{
+    transition: all 0.7s ease;
+}
+.user-list-enter-from,
+.user-list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+}
+.user-list-move {
+    transition: transform 0.4s ease;
 }
 </style>
